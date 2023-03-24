d3dea823cc7139b17817da4bed2dab70f5946b9a
1dam <victor.moreno7@educa.madrid.org>
2023-03-24 09:10:29 +0100

    tyt

--- JavaScrip/example2/JS/ej1.js
+++ JavaScrip/example2/JS/ej1.js
@@ -2,7 +2,8 @@
 document.getElementById("Itext").addEventListener("keyup", toUpper);
 document.getElementById("Itext").addEventListener("focusout", validateLength);
 document.getElementById("IEmail").addEventListener("focusout", validateEmail);
-
+document.getElementById("button-cancel").addEventListener("click", resetForm);
+document.getElementById("button-cancel").addEventListener("click", validate);
 
 
 function toUpper() {
@@ -11,9 +12,9 @@ function toUpper() {
 }
 function validateLength() {
     let color=document.getElementById("Itext");
-    if (color.value.length<2 && color.value.length>25){
+    if (color.value.length<2 || color.value.length>25){
         color.style.backgroundColor="red";
-    } else if(color.value.length>2 && color.value.length<25){
+    } else if(color.value.length>=2 && color.value.length<=25){
         color.style.backgroundColor="white";
     }
 }
@@ -26,4 +27,16 @@ function validateEmail() {
     } else if(mail.checkValidity() == true){
         mail.style.backgroundColor="white";
     }
+}
+
+function resetForm(ev) {
+    ev.preventDefault();
+    let answer = confirm("Are you sure that you want to reset all the data?");
+    if (answer) {
+        document.getElementById("form-user").reset();
+    }
+}
+
+function validate(ev) {
+    ev.preventDefault();
 }
