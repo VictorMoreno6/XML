8b2cd8c3f2e6770bd6276ec7e435e95558e16613
1dam <victor.moreno7@educa.madrid.org>
2023-03-29 12:31:55 +0200

    uu

--- JavaScrip/JS/form1dam.js
+++ JavaScrip/JS/form1dam.js
@@ -10,5 +10,34 @@ function resetForm(ev) {
 }
 
 function validate(ev) {
+    let listErrors=[];
     ev.preventDefault();
+    let first=document.getElementById("input-first");
+    if(first.value.length<3 || first.value.length>8) {
+        listErrors.push("The name must be between 3 to 8 characters long");
+    }
+    
+    let chk=document.getElementById("input-alive");
+    if(!chk.checked){
+        listErrors.push("The person is not alive");
+    }
+    let age=document.getElementById("input-age");
+
+    if(age.selectedIndex===0){
+        listErrors.push("Please select an age");
+    }
+    let email=document.getElementById("input-email");
+
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))) {
+        listErrors.push("Email not valid");
+    }
+
+    if(listErrors.length===0){
+        document.getElementById("form-user").submit();
+    }
+    else{
+        let tbM= document.getElementById("errorMessage");
+        listErrors.forEach (err => {tbM.innerHTML +- "<p>" + err +"<p>"})
+        
+    }
 }
