ddaa3f23ebb894b6a1482627751986af9dac42aa
1dam <victor.moreno7@educa.madrid.org>
2023-04-27 15:13:46 +0200

    rr

--- JavaScrip/FinalAssigment/JS/final.js
+++ JavaScrip/FinalAssigment/JS/final.js
@@ -1,6 +1,10 @@
 document.getElementById("button").addEventListener("click", startGame);
+let sequence=[];
+let cont=0;
+let roundNr=1;
 
 function startGame(){
+    //let sequence=[];
     let height=document.getElementById("rows").value;
     let width=document.getElementById("col").value;
     let level= document.getElementById("level").value;
@@ -13,11 +17,13 @@ function startGame(){
     document.getElementById("message").innerText="PLAYING...";
     drawField(width,height);
     generateSequence(width, height, level);
+    
+    playSequence(roundNr)
 }
 
 function drawField(width,height){
     let vgrid = document.getElementById("field");
-    const colors = ["red", "blue", "green", "yellow", "purple", "orange", "black", "white", "gray", "pink", "brown", "beige", "teal", "navy", "maroon", "olive", "lime", "aqua", "fuchsia", "silver", "gold", "indigo", "turquoise", "plum", "magenta"];
+    const colors = ["brown", "blue", "green", "yellow", "purple", "orange", "black", "white", "gray", "pink", "brown", "beige", "teal", "navy", "maroon", "olive", "lime", "aqua", "fuchsia", "silver", "gold", "indigo", "turquoise", "plum", "magenta"];
     let C = 0;
     for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++, C++) {
@@ -51,6 +57,35 @@ function disableEventsField() {
 
 function generateSequence(width, height, level){
     let number= level*5;
-    let colum= Math.floor(Math.random() * (width - 0 + 1));
-    let row= Math.floor(Math.random() * (height - 0 + 1));
+    for (n=0;n<number;n++) {
+            let x= Math.floor(Math.random() * (width));
+            let y= Math.floor(Math.random() * (height));
+            sequence.push([x,y]);
+    }
+}
+
+function playSequence(roundNr){
+   
+    const color=[];
+    let myInterval= setInterval(showElement,1000, roundNr,color);
+    
+    
+}
+
+function showElement(roundNr, color){
+        let[x,y]=sequence[cont];
+        console.log("aa")
+        let button=document.getElementById(y + "_" + x);
+        color[cont]=button.style.backgroundColor;    
+        button.style.backgroundColor="red";
+        cont++;
+        if(cont>0){
+            let i=cont;
+            button.style.backgroundColor=color[i-1];
+        }
+        
+    
+
+   
+
 }
